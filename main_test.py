ca28afaf53b9791d8e1b7bddb91f08fd725681b4
Giga2roo <sayhoahn@naver.com>
2022-08-30 23:07:08 +0900

    fix typo in main_test.py

--- main_test.py
+++ main_test.py
@@ -3,7 +3,7 @@ import unittest
 from main import hello
 
 
-class MainTest(unittest, TestCase):
+class MainTest(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello(), "Hello World!")
 
